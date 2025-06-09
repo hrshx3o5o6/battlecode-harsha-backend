b1dcdc94f0b65a9160020420bc25f1160924c964
hrshx3o5o6 <hkangala@gmail.com>
2025-06-09 23:57:03 +0530

    smol change

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.get('/', (req, res) => {
   res.send('Judge0 Playground is live!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
